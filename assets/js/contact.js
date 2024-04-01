07130472da97bfcad7f18dc658a4dc9a29634d49
“flakronademi” <flakronflaki0@gmail.com>
2024-04-01 14:12:02 +0200

    changed : contact form to use google sheet instead of formspree , commented temporary our work descriptions

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,12 +1,19 @@
-// i did it this way because it redirects to formspree.io page and i did this method to prevent the redirect
-function submitForm(event) {
-    event.preventDefault(); 
-    var form = document.getElementById('contactForm');
-    var formData = new FormData(form);
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://formspree.io/f/myyrnpkl', true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    successMessage.innerHTML = 'Form submitted successfully!';
-    xhr.send(new URLSearchParams(formData));
-    form.reset();
-  }
+const form = document.querySelector("#form")
+const submitButton = document.querySelector("#submit")
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyds9eqEWaQqJ0kUH0kBFJqGxfvMcJNIMb_vOgWXxgrBCqjXOGZD-jKHP7lCsdMnEG1pQ/exec';
+
+form.addEventListener('submit', e => {
+  submitButton.disabled = true
+  e.preventDefault()
+  let requestBody = new FormData(form)
+  fetch(scriptURL, { method: 'POST', body: requestBody})
+    .then(response => {
+    document.getElementById("successMessage").textContent = "Form submitted successfully!";
+    submitButton.disabled = false
+      })
+    .catch(error => {
+    document.getElementById("successMessage").textContent = "Error submitting form!";
+    submitButton.disabled = false
+    }
+    )
+})
