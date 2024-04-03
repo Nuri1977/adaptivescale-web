358cd43f77f0fb11d0986e733522a8a92b7f58b4
“flakronademi” <flakronflaki0@gmail.com>
2024-04-03 13:16:19 +0200

    added loader in contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,19 +1,22 @@
 const form = document.querySelector("#form")
 const submitButton = document.querySelector("#submit")
+const loader = document.querySelector("#loader") 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyds9eqEWaQqJ0kUH0kBFJqGxfvMcJNIMb_vOgWXxgrBCqjXOGZD-jKHP7lCsdMnEG1pQ/exec';
 
 form.addEventListener('submit', e => {
-  submitButton.disabled = true
+  loader.style.display = "block";
+  submitButton.style.display ="none"
   e.preventDefault()
   let requestBody = new FormData(form)
   fetch(scriptURL, { method: 'POST', body: requestBody})
     .then(response => {
-    document.getElementById("successMessage").textContent = "Form submitted successfully!";
-    submitButton.disabled = false
-      })
+      document.getElementById("successMessage").textContent = "Form submitted successfully!";
+      loader.style.display = "none";
+      submitButton.style.display ="block"
+    })
     .catch(error => {
-    document.getElementById("successMessage").textContent = "Error submitting form!";
-    submitButton.disabled = false
-    }
-    )
-})
+      document.getElementById("successMessage").textContent = "Error submitting form!";
+      loader.style.display = "none";
+      submitButton.style.display ="block"
+    });
+});
